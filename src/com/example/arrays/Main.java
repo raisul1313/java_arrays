16f2b90440449b4e1ece7ce94c85ab10691f831a
raisul1313 <raisul.islam1313@gmail.com>
2021-11-30 15:53:49 +0600

    Initial commit

---
+++ src/com/example/arrays/Main.java
@@ -0,0 +1,24 @@
+package com.example.arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] students = new String[5];
+        students[0] = "Raisul";
+        students[1] = "Rahim";
+        students[2] = "Karim";
+        students[3] = "Rafiq";
+        students[4] = "Sarah";
+//        System.out.println(students[2]);
+
+        String[] employes = {"A", "B", "C"};
+        int[] number = {1, 2, 3, 4, 5, 6};
+        for (int i = 0; i < 6; i++) {   //when array size is fixed;
+            System.out.println(number[i]);
+        }
+
+        int[] number1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < number1.length; i++) {    //if we don't know te size of array
+            System.out.println(number1[i]);
+        }
+    }
+}
